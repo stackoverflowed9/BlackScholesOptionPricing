a1d0ef0eb9d552c351cd9192bb1d519dddf4f9a2
stackoverflowed9 <arjuncoder27@gmail.com>
2026-02-16 16:11:50 +0530

    Used yfinance for retrieving real past data

--- bsmOptionPricing.py
+++ bsmOptionPricing.py
@@ -1,44 +1,59 @@
+import yfinance as yf
+import pandas as pd
 import numpy as np
 from scipy.stats import norm
+from datetime import datetime
+
+r = 0.05  # trial risk-free rate
+
+expiry = datetime(2026, 3, 13)
+today = datetime.today()
+T = (expiry-today).days/365
 
 
-S0 = 100
-K = 100
-r = 0.05
-T = 1
-Cmarket = 10
+ticker = yf.Ticker("AAPL")
+
+spot = ticker.history(period="1d")["Close"].iloc[-1]
+options = ticker.option_chain(expiry.strftime("%Y-%m-%d"))
+calls = options.calls
+puts = options.puts
 
 
-def black_scholes_call(S0, K, r, vol, T):
+def black_scholes_call(S0, K, vol, r, T):
     d1 = (np.log(S0/K) + (r + 0.5*(vol**2))*T)/(vol*np.sqrt(T))
     d2 = d1 - vol*np.sqrt(T)
 
     return S0*norm.cdf(d1) - K*np.exp(-1*r*T)*norm.cdf(d2)
 
 
-def black_scholes_put(S0, K, r, vol, T):
+def black_scholes_put(S0, K, vol, r, T):
     d1 = (np.log(S0/K) + (r + 0.5*(vol**2))*T)/(vol*np.sqrt(T))
     d2 = d1 - vol*np.sqrt(T)
 
     return K*np.exp(-1*r*T)*norm.cdf(-1*d2) - S0*norm.cdf(-1*d1)
 
 
-def f(vol):
-    return black_scholes_call(S0, K, r, vol, T) - Cmarket
+def impliedVolatility(C_market, S, K, r, T, option_type, tol=1e-3, init=0.2):
+    vol = init
+    price = tol + C_market + 1
+    while (abs(price-C_market) > tol):
+        if (option_type=="call"):
+            price = black_scholes_call(S, K, vol, r, T)
+        else :
+            price = black_scholes_put(S, K, vol, r, T)
+
+        vega = S*norm.pdf((np.log(S/K)+(r+0.5*vol**2)*T)/(vol*np.sqrt(T)))*np.sqrt(T)
+        vol -= (price - C_market)/vega
+    return vol
 
 
-def df(vol):
-    d1 = (np.log(S0/K) + (r + 0.5*(vol**2))*T)/(vol*np.sqrt(T))
-    return S0*norm.pdf(d1)*np.sqrt(T)
+calls["iv"] = calls.apply(lambda row: impliedVolatility(row["lastPrice"],spot,row["strike"],r, T,"call"),axis=1)
+puts["iv"] = puts.apply(lambda row: impliedVolatility(row["lastPrice"],spot,row["strike"],r,T,"put"),axis=1)
 
 
-def impliedVolatility(a, iter):
-    while iter>0:               #Use Newton-Raphson on BS eqn to compute implied volatility
-        a = a - f(a)/df(a)
-        iter-=1
+print("Spot Price:", spot)
+print("Calls with IV:")
+print(calls[["strike", "lastPrice", "iv"]].head())
 
-    return a
-
-
-iv = impliedVolatility(1, 3)
-print(iv)
+print("\nPuts with IV:")
+print(puts[["strike", "lastPrice", "iv"]].head())
